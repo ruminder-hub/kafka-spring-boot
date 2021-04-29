44e02ced67bf649f66b68e2ac7144f831a626bea
Ruminder Singh <rumindersingh@Ruminders-MacBook-Pro.local>
2021-04-28 23:06:19 -0300

    Eureka server added

---
+++ kafka-eureka/src/test/java/com/ruminderhub/kafkaeureka/KafkaEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ruminderhub.kafkaeureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
